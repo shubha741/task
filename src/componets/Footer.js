b84777a888b3890f6dcd126b4ea64299ce79a689
shobha <shobaa1996@gmail.com>
2022-05-02 09:37:49 +0530

    first commit

---
+++ src/componets/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+ function Footer() {
+  return (
+    <div className='row bg-dark'>
+
+        <div className='col-6   '>
+         <small>Basic Template fetch Data using Api</small>
+        </div>
+        <div className='col-6'>
+     <p> @copyright 2022, Design and Developred By  Shobha</p>
+        </div>
+    </div>
+  )
+}
+export default Footer
